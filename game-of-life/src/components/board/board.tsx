04bc505b7bae651d211a4d4be92429fe52218aa7
Sam Rosenthal <samrrosenthal@gmail.com>
2020-06-27 12:31:05 -0700

    breaking things with typescript

--- game-of-life/src/components/board/board.tsx
+++ game-of-life/src/components/board/board.tsx
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
+import Cell from "./cell/cell.tsx";
 
 const numRows = 50;
 const numCols = 50;
 
-function Board() {}
+function Board() {
+    const [board: Cell[][], makeBoard: function] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => <Cell />));
+    }
+  });
+
+  return <div>{board.map((i: Cell) => i)}</div>;
+}
--- game-of-life/src/components/board/cell/cell.tsx
+++ game-of-life/src/components/board/cell/cell.tsx
@@ -15,3 +15,5 @@ function Cell() {
     ></div>
   );
 }
+
+export default Cell;
