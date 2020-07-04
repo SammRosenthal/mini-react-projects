031c7fc1a86f3f2c9868559e2551d9c032326410
Sam Rosenthal <samrrosenthal@gmail.com>
2020-07-04 11:31:12 -0700

    parsing logic into multiple components

--- game-of-life/src/components/grid/Grid.tsx
+++ game-of-life/src/components/grid/Grid.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Cell from "../board/cell/cell";
 
-function Grid() {
-  return <h1>Grid</h1>;
+function Grid(props: {
+  gridFull: Array<Array<boolean>>;
+  rows: number;
+  cols: number;
+  selectBox: object;
+}) {
+  const width = props.cols * 14;
+  let rowsArr = [];
+
+  let boxClass = "";
+  for (let i = 0; i < props.rows; i++) {
+    for (let j = 0; j < props.cols; j++) {
+      let boxId = i + " " + j;
+      boxClass = props.gridFull[i][j] ? "box on" : "box off";
+      rowsArr.push(
+        <Cell
+          boxClass={boxClass}
+          key={boxId}
+          boxId={boxId}
+          row={i}
+          col={j}
+          selectBox={props.selectBox}
+        />
+      );
+    }
+  }
+
+  return (
+    <h1 className="grid" style={{ width: width }}>
+      {{ rowsArr }}
+    </h1>
+  );
 }
 
 export default Grid;
