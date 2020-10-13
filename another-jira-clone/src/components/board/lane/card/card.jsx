1323fac5387e2e82a87489b3aca100979c1c81e2
Sam Rosenthal <samrrosenthal@gmail.com>
2020-10-13 14:24:47 -0700

    card state added

--- another-jira-clone/src/components/board/lane/card/card.jsx
+++ another-jira-clone/src/components/board/lane/card/card.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './card.scss';
 
 // core features
-// story name
-// story points
-// story status
-// assigned user
+    // story name
+    // story points
+    // story status
+    // assigned user
 
-function Card() {
+function Card(props) {
+    const [title, setTitle] = useState("Sam's User Story");
+    const [status, setStatus] = useState("In Progress");
+    const [points, setPoints] = useState("3");
+    const [user, setUser] = useState("Sam Rosenthal");
+
     return(
         <div className="cardContainer">
-            this is a card
+            <div className="cardHeader">
+                {title}
+            </div>
+            <div className="cardStatus">
+                {status}
+            </div>
+            <div className="cardInfo">
+                {user}
+                {points}
+            </div>
         </div>
     )
 }
