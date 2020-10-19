6bfd7612ee0df2f4711faa75c236c2dd3963b1c0
Sam Rosenthal <samrrosenthal@gmail.com>
2020-10-18 18:29:58 -0700

    modal popping up

--- another-jira-clone/src/App.js
+++ another-jira-clone/src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import Board from './components/board/board';
-import Header from './components/navbar/navbar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import 'fontsource-roboto';
+import React, { useState } from "react";
+import Board from "./components/board/board";
+import Header from "./components/navbar/navbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import "fontsource-roboto";
 
 function App() {
+  const [cards, addCard] = useState([]);
+
+  const createCard = () => {};
+
   return (
     <>
-        <CssBaseline />
-        <Header />
-        <Board/>
+      <CssBaseline />
+      <Header />
+      <Board />
     </>
   );
 }
--- another-jira-clone/src/components/navbar/navbar.jsx
+++ another-jira-clone/src/components/navbar/navbar.jsx
@@ -1,20 +1,70 @@
-import React from 'react';
-import './navbar.scss';
-import {AppBar, Toolbar, Typography, Button} from '@material-ui/core';
-import {MenuIcon} from '@material-ui/icons';
+import React, { useState } from "react";
+import "./navbar.scss";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { MenuIcon } from "@material-ui/icons";
+import Modal from "@material-ui/core/Modal";
 
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyles() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%;)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Header() {
-    return (
-        <AppBar position="sticky" color="secondary" >
-          <Toolbar className="navbarContent">
-            <Typography variant="h6" >
-                Another Jira Clone
-            </Typography>
-            <Button color="inherit">Create</Button>
-          </Toolbar>
-        </AppBar>
-    )
+  const classes = useStyles();
+  const [modalStyle, getModalStyle] = useState(getModalStyles);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const modalBody = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="modal-title">Card Information</h2>
+      <p id="modal-description">this is the body</p>
+      <Header />
+    </div>
+  );
+
+  return (
+    <AppBar position="sticky" color="secondary">
+      <Toolbar className="navbarContent">
+        <Typography variant="h6">Another Jira Clone</Typography>
+        <Button onClick={handleOpen} color="inherit">
+          Create
+        </Button>
+      </Toolbar>
+      <Modal open={open} onClose={handleClose}>
+        <h1>test</h1>
+      </Modal>
+    </AppBar>
+  );
 }
 
 export default Header;
