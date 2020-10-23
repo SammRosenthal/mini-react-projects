e5575b40d43f0cf6ee42a9b94d174d55e3860022
Sam Rosenthal <samrrosenthal@gmail.com>
2020-10-23 11:55:14 -0700

    create card modal popup complete

--- another-jira-clone/src/components/navbar/navbar.jsx
+++ another-jira-clone/src/components/navbar/navbar.jsx
@@ -2,21 +2,44 @@ import React, { useState } from "react";
 import "./navbar.scss";
 import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { MenuIcon } from "@material-ui/icons";
+import TextField from '@material-ui/core/TextField';
 import Modal from "@material-ui/core/Modal";
 
 const useStyles = makeStyles((theme) => ({
   center: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
     position: "absolute",
     left: "50%",
     top: "50%",
     transform: "translate(-50%, -50%)",
-    height: "50vh",
-    width: "75vw",
+    height: "80vh",
+    width: "800px",
     backgroundColor: "#f4f5f7",
-    borderRadius: "1%",
+    borderRadius: ".5%",
     border: "2px solid black",
-    outline: "none"
+    outline: "none",
+    padding: "0 32px"
+  },
+  cardGeneralInformation: {
+    display: "flex",
+    justifyContent: "space-evenly"
+  },
+  cardContent: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    width: "732px",
+    height: "500px"
+  },
+  textBox: {
+    width: "100%",
+  },
+  footer: {
+    display: "flex",
+    justifyContent: "flex-end"
   }
 }));
 
@@ -34,8 +57,33 @@ function Header() {
 
   const modalBody = (
     <div className={classes.center}>
-      <h2 id="modal-title">Card Information</h2>
-      <p id="modal-description">this is the body</p>
+      <div id="cardCreationHeader"><h1>Create Card</h1></div>
+      <div id="cardGeneralInformation" className={classes.cardGeneralInformation}>
+        <TextField id="assignee" label="Assignee" />
+        <TextField id="points" label="Story Points" />
+        <TextField id="status" label="Status" />
+      </div>
+      <div id="cardContent" className={classes.cardContent}>
+        <TextField
+          multiline
+          rows={10}
+          variant="outlined"
+          id="acceptanceCriteria"
+          label="Acceptance Criteria"
+          className={classes.textBox}
+        />
+        <TextField
+          multiline
+          rows={10}
+          variant="outlined"
+          id="description"
+          label="Description"
+          className={classes.textBox}
+        />
+      </div>
+      <div id="cardCreationFooter" className={classes.footer}>
+        <Button variant="contained" color="primary">Create</Button>
+      </div>
     </div>
   );
 
