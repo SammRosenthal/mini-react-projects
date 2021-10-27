30bc7a6f58504cd757cdfb5566c10323629db30a
Sam Rosenthal <samrrosenthal@gmail.com>
2021-10-26 20:26:20 -0700

    init

---
+++ jira-clone/src/App.tsx
@@ -0,0 +1,13 @@
+import Lane from './components/lane/Lane';
+
+function App() {
+  return (
+    <div className="md:flex-row h-auto flex items-center flex-col">
+      <Lane />
+      <Lane />
+      <Lane />
+    </div>
+  );
+}
+
+export default App;
