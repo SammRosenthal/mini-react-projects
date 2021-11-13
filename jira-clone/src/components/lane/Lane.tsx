371ddd90a30d7cb1ce6f68dd69b4fa87661a7df9
Sam Rosenthal <samrrosenthal@gmail.com>
2021-11-12 21:02:23 -0800

    changes

--- jira-clone/src/components/lane/Lane.tsx
+++ jira-clone/src/components/lane/Lane.tsx
@@ -8,9 +8,10 @@ interface LaneProps {
 
 function Lane({ title, cards }: LaneProps) {
   return (
-    <div className="md:ml-2 md:mr-2 md:max-h-9/10 md:min-h-9/10 md:min-w-350 my-5 rounded-lg w-4/5 max-w-350 max-h-4/5 min-h-1/2 shadow p-2 bg-gray-100">
+    <div className="md:ml-2 md:mr-2 md:max-h-9/10 md:min-h-9/10 md:min-w-350 my-5 rounded-lg w-4/5 max-w-350 max-h-4/5 min-h-1/2 shadow p-2 pb-0 bg-gray-100">
       <div className="text-center text-xl sticky bg-gray-100 h-5p"><h1>{title}</h1></div>
       <div className="overflow-auto h-95p">
+        {/* temp for dev */}
         {
           Array.from(Array(cards)).map((v, i) => {
             return <Card key={i} />
